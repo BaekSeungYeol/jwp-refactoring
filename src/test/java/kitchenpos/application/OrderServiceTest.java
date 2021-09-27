2c8bc6f965d7ad174c914db722f1301468a922b1
백승열 <beck33333@naver.com>
2021-09-28 00:59:08 +0900

    Test: 주문 등록 시 예외 테스트 추가

--- src/test/java/kitchenpos/application/OrderServiceTest.java
+++ src/test/java/kitchenpos/application/OrderServiceTest.java
@@ -15,12 +15,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.swing.text.html.Option;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import static java.time.LocalDate.now;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.hamcrest.Matchers.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
@@ -59,21 +61,13 @@ public class OrderServiceTest {
 
     @BeforeEach
     void setUp() {
-        //   (Long tableId, String orderStatus, LocalDateTime localDateTime, List<OrderLineItem> orderLineItems) {
-        /*
-            private Long id;
-    private Long tableGroupId;
-    private int numberOfGuests;
-    private boolean empty;
-         */
-        // 		savedOrderTable = TestDomainConstructor.orderTableWithId(1L, 2, false, NOT_EMPTY_TABLE_ID);
-        savedOrderTable = Fixtures.orderTableWithId(1L,2, false, NOT_EMPTY_TABLE_ID);
+        savedOrderTable = Fixtures.orderTableWithId(1L, 2, false, NOT_EMPTY_TABLE_ID);
         orderLineItem1 = Fixtures.orderLineItem(null, MENU_ID, 2);
         orderLineItem2 = Fixtures.orderLineItem(null, MENU2_ID, 1);
         orderLineItems = Arrays.asList(orderLineItem1, orderLineItem2);
         orderLineItemsSize = orderLineItems.size();
-        createdOrder = Fixtures.orderWithId(NOT_EMPTY_TABLE_ID, OrderStatus.COOKING.name(),LocalDateTime.now(),
-                orderLineItems,NEW_ORDER_ID);
+        createdOrder = Fixtures.orderWithId(NOT_EMPTY_TABLE_ID, OrderStatus.COOKING.name(), LocalDateTime.now(),
+                orderLineItems, NEW_ORDER_ID);
     }
 
 
@@ -83,8 +77,8 @@ public class OrderServiceTest {
 
         // given
         String orderStatus = OrderStatus.COOKING.name();
-        OrderLineItem savedOrderLineItem1 = Fixtures.orderLineItemWithSeq(NEW_ORDER_ID,MENU_ID,2,1L);
-        OrderLineItem savedOrderLineItem2 = Fixtures.orderLineItemWithSeq(NEW_ORDER_ID, MENU2_ID,1,2L);
+        OrderLineItem savedOrderLineItem1 = Fixtures.orderLineItemWithSeq(NEW_ORDER_ID, MENU_ID, 2, 1L);
+        OrderLineItem savedOrderLineItem2 = Fixtures.orderLineItemWithSeq(NEW_ORDER_ID, MENU2_ID, 1, 2L);
 
         when(menuDao.countByIdIn(MENU_IDS)).thenReturn(orderLineItemsSize);
         when(orderTableDao.findById(anyLong())).thenReturn(Optional.ofNullable(savedOrderTable));
@@ -94,7 +88,7 @@ public class OrderServiceTest {
                 , hasProperty("orderStatus", is(orderStatus))
                 , hasProperty("orderedTime", notNullValue())
         )))).thenReturn(createdOrder);
-        when(orderLineItemDao.save(any())).thenReturn(savedOrderLineItem1,savedOrderLineItem2);
+        when(orderLineItemDao.save(any())).thenReturn(savedOrderLineItem1, savedOrderLineItem2);
         // when
         Order result = orderService.create(createdOrder);
 
@@ -110,4 +104,39 @@ public class OrderServiceTest {
         assertThat(result.getOrderedTime()).isNotNull();
         assertThat(result.getOrderLineItems()).containsExactlyInAnyOrder(savedOrderLineItem1, savedOrderLineItem2);
     }
+
+    @Test
+    @DisplayName("주문 등록 시, 주문 아이템이 Null이면 IllegalArgumentException을 throw 해야한다.")
+    void createOrderItemNull() {
+        //given
+        Order nullItemOrder = Fixtures.order(NOT_EMPTY_TABLE_ID, null, null, null);
+
+        //when-then
+        assertThatThrownBy(() -> orderService.create(nullItemOrder))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("주문 등록 시 , 주문 아이템이 비어있다면 IllegalArgumentException을 throw 한다.")
+    void createOrderItemEmpty() {
+        Order emptyItemOrder = Fixtures.order(NOT_EMPTY_TABLE_ID, null, null, new ArrayList<>());
+
+        assertThatThrownBy(() -> orderService.create(emptyItemOrder))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("주문 등록 시, 주문 테이블이 등록되어 있지 않다면 IllegalArgumentException을 throw 해야 한다.")
+    void createNotExistOrderTable() {
+        Order notExistOrderTable = Fixtures.order(100L, null, null, orderLineItems);
+        when(menuDao.countByIdIn(MENU_IDS)).thenReturn(orderLineItemsSize);
+        when(orderTableDao.findById(100L)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> orderService.create(notExistOrderTable))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+
 }
+
