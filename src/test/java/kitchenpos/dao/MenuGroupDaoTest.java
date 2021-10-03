85b8287d7ee0e11a5e59936dbb93e6b134b59351
백승열 <beck33333@naver.com>
2021-10-03 22:08:49 +0900

    Test : 메뉴 그룹 요구사항 MenuGroupDaoTest 추가

---
+++ src/test/java/kitchenpos/dao/MenuGroupDaoTest.java
@@ -0,0 +1,47 @@
+package kitchenpos.dao;
+
+import kitchenpos.BaseDaoTest;
+import kitchenpos.domain.Fixtures;
+import kitchenpos.domain.MenuGroup;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class MenuGroupDaoTest extends BaseDaoTest {
+
+    @Autowired
+    MenuGroupDao menuGroupDao;
+
+    @Test
+    @DisplayName("메뉴 그룹을 정상적으로 등록하면 ID를 반환한다.")
+    void createMenuGroup() {
+
+        // given
+        String name = "메뉴그룹1";
+        MenuGroup menuGroup = Fixtures.menuGroup(name);
+
+        // when
+        MenuGroup savedMenuGroup = menuGroupDao.save(menuGroup);
+
+        // then
+        assertThat(savedMenuGroup.getId()).isNotNull();
+        assertThat(savedMenuGroup.getName()).isEqualTo(name);
+    }
+
+    @Test
+    @DisplayName("메뉴 그룹의 이름이 없으면 DataIntegrityViolationException을 throw 한다.")
+    void createMenuGroupWithoutName() {
+        // given
+        MenuGroup menuGroup = Fixtures.menuGroup(null);
+
+        // when
+        assertThatThrownBy(() -> menuGroupDao.save(menuGroup))
+                .isInstanceOf(DataIntegrityViolationException.class);
+
+    }
+
+}
